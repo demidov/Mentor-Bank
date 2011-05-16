d0ca4a4d3f208e289ecc1370efea005cab6ad5d7
U-ITECO\demidov <demidov@demidov-pc.msk.i-teco.ru>
2011-05-16 18:54:08 +0400

    commit

--- src/backoffice-model/test/ru/mentorbank/backoffice/services/moneytransfer/MoneyTransferServiceTest.java
+++ src/backoffice-model/test/ru/mentorbank/backoffice/services/moneytransfer/MoneyTransferServiceTest.java
@@ -1,36 +1,76 @@
 package ru.mentorbank.backoffice.services.moneytransfer;
 
-import static org.junit.Assert.fail;
+import static org.mockito.Mockito.*;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import ru.mentorbank.backoffice.dao.OperationDao;
+import ru.mentorbank.backoffice.dao.exception.OperationDaoException;
+import ru.mentorbank.backoffice.dao.stub.OperationDaoStub;
+import ru.mentorbank.backoffice.model.Operation;
+import ru.mentorbank.backoffice.model.stoplist.*;
+import ru.mentorbank.backoffice.model.transfer.*;
+import ru.mentorbank.backoffice.services.accounts.AccountService;
+import ru.mentorbank.backoffice.services.accounts.AccountServiceBean;
 import ru.mentorbank.backoffice.services.moneytransfer.exceptions.TransferException;
+import ru.mentorbank.backoffice.services.stoplist.StopListService;
+import ru.mentorbank.backoffice.services.stoplist.StopListServiceStub;
 import ru.mentorbank.backoffice.test.AbstractSpringTest;
 
 public class MoneyTransferServiceTest extends AbstractSpringTest {
 
 	@Autowired
 	private MoneyTransferServiceBean moneyTransferService;
+	private JuridicalAccountInfo srcAccountInfo;
+	private TransferRequest transferRequest;
+	private PhysicalAccountInfo dstAccountInfo;
+	private StopListInfo stopListInfo;
 
 	@Before
 	public void setUp() {
+		srcAccountInfo = new JuridicalAccountInfo();
+		srcAccountInfo.setAccountNumber("1");
+		srcAccountInfo.setInn(StopListServiceStub.INN_FOR_OK_STATUS);
+
+		dstAccountInfo = new PhysicalAccountInfo();
+		dstAccountInfo.setAccountNumber("2");
+		dstAccountInfo.setDocumentNumber(StopListServiceStub.DOCUMENT_NUMBER_FOR_OK_STATUS);
+		dstAccountInfo.setDocumentSeries("2-2");
+		dstAccountInfo.setFirstname("Ivanov");
+		dstAccountInfo.setLastname("Ivan");
+		dstAccountInfo.setMiddlename("Ivanovich");
+
+		transferRequest = new TransferRequest();
+		transferRequest.setSrcAccount(srcAccountInfo);
+		transferRequest.setDstAccount(dstAccountInfo);
+
+		stopListInfo = new StopListInfo();
+		stopListInfo.setStatus(StopListStatus.OK);
+		stopListInfo.setComment("Comment");
 	}
 
 	@Test
-	public void transfer() throws TransferException {
-		fail("not implemented yet");
-		// TODO: Необходимо протестировать, что для хорошего перевода всё
-		// работает и вызываются все необходимые методы сервисов
-		// Далее следует закоментированная закотовка
-		// StopListService mockedStopListService =
-		// mock(StopListServiceStub.class);
-		// AccountService mockedAccountService = mock(AccountServiceBean.class);
-		//
-		// moneyTransferService.transfer(null);
-		//
-		// verify(mockedStopListService).getJuridicalStopListInfo(null);
-		// verify(mockedAccountService).verifyBalance(null);
+	public void transfer() throws TransferException, OperationDaoException {
+		StopListService mockedStopListService = mock(StopListServiceStub.class);
+		when(mockedStopListService.getJuridicalStopListInfo(any(JuridicalStopListRequest.class))).thenReturn(stopListInfo);
+		when(mockedStopListService.getPhysicalStopListInfo(any(PhysicalStopListRequest.class))).thenReturn(stopListInfo);
+
+		AccountService mockedAccountService = mock(AccountServiceBean.class);
+		when(mockedAccountService.verifyBalance(srcAccountInfo)).thenReturn(true);
+
+		OperationDao mockedOperationDao = mock(OperationDaoStub.class);
+
+		moneyTransferService.setAccountService(mockedAccountService);
+		moneyTransferService.setStopListService(mockedStopListService);
+		moneyTransferService.setOperationDao(mockedOperationDao);
+
+		moneyTransferService.transfer(transferRequest);
+
+		verify(mockedAccountService).verifyBalance(srcAccountInfo);
+		verify(mockedStopListService).getJuridicalStopListInfo(any(JuridicalStopListRequest.class));
+		verify(mockedStopListService).getPhysicalStopListInfo(any(PhysicalStopListRequest.class));
+		verify(mockedOperationDao).saveOperation(any(Operation.class));
 	}
 }
--- src/backoffice-model/test/ru/mentorbank/backoffice/services/stoplist/StopListServiceStub.java
+++ src/backoffice-model/test/ru/mentorbank/backoffice/services/stoplist/StopListServiceStub.java
@@ -1,9 +1,6 @@
 package ru.mentorbank.backoffice.services.stoplist;
 
-import ru.mentorbank.backoffice.model.stoplist.JuridicalStopListRequest;
-import ru.mentorbank.backoffice.model.stoplist.PhysicalStopListRequest;
-import ru.mentorbank.backoffice.model.stoplist.StopListInfo;
-import ru.mentorbank.backoffice.model.stoplist.StopListStatus;
+import ru.mentorbank.backoffice.model.stoplist.*;
 import ru.mentorbank.backoffice.services.stoplist.StopListService;
 
 public class StopListServiceStub implements StopListService {
@@ -11,6 +8,10 @@ public class StopListServiceStub implements StopListService {
 	public static final String INN_FOR_OK_STATUS = "1111111111111";
 	public static final String INN_FOR_STOP_STATUS = "22222222222222";
 	public static final String INN_FOR_ASKSECURITY_STATUS = "33333333333333";
+	
+	public static final String DOCUMENT_NUMBER_FOR_OK_STATUS = "111";
+	public static final String DOCUMENT_NUMBER_FOR_STOP_STATUS = "222";
+	public static final String DOCUMENT_NUMBER_FOR_ASKSECURITY_STATUS = "333";
 
 	@Override
 	public StopListInfo getJuridicalStopListInfo(
@@ -29,8 +30,16 @@ public class StopListServiceStub implements StopListService {
 
 	@Override
 	public StopListInfo getPhysicalStopListInfo(PhysicalStopListRequest request) {
-		//TODO: Реализовать
-		return null;
+		StopListInfo stopListInfo = new StopListInfo();
+		stopListInfo.setComment("Комментарий");
+		if (DOCUMENT_NUMBER_FOR_OK_STATUS.equals(request.getDocumentNumber())){
+			stopListInfo.setStatus(StopListStatus.OK);
+		} else if(DOCUMENT_NUMBER_FOR_STOP_STATUS.equals(request.getDocumentNumber())){
+			stopListInfo.setStatus(StopListStatus.STOP);
+		} else {
+			stopListInfo.setStatus(StopListStatus.ASKSECURITY);
+		}
+		return stopListInfo;
 	}
 
 }
